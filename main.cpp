a7a5377c0b2336045456bd241526879ec6e6e508
Rhaven Kurosaia <105242602+Rhaven3@users.noreply.github.com>
2024-05-21 10:35:21 +0200

    fault sstart

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
@@ -8,4 +9,7 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
     return a.exec();
+
+    string
+
 }
