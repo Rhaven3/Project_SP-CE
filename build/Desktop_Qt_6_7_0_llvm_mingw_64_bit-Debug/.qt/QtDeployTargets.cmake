9009d66e5b49db4fa44312bd74bf2dded3bf9dea
Rhaven Kurosaia <105242602+Rhaven3@users.noreply.github.com>
2024-05-21 09:24:40 +0200

    add project

---
+++ build/Desktop_Qt_6_7_0_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Project_SP-CE_FILE C:/Users/fontaine/Desktop/Théo Fontaine/Projet BTS/Project_SP-CE/Project_SP-CE/build/Desktop_Qt_6_7_0_llvm_mingw_64_bit-Debug/Project_SP-CE.exe)
+set(__QT_DEPLOY_TARGET_Project_SP-CE_RUNTIME_DLLS C:/Qt/6.7.0/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/llvm-mingw_64/bin/Qt6Core.dll)
