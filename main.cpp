15877d9c2c74ce978e05c505de9d8db8e8858844
Rhaven Kurosaia <105242602+Rhaven3@users.noreply.github.com>
2024-05-21 16:34:51 +0200

    debug qt

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include "mainwindow.h"
-#include <iostream>
-#include <cstdio>
+
 #include <QApplication>
-using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-    freopen( "output.txt", "w", stdout );
-    freopen( "error.txt", "w", stderr );
-
-    cout << "Output message" << endl;
-    cerr << "Error message" << endl;
-
     return a.exec();
 }
