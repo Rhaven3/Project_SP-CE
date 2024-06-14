32dd73a650be47ae0b92d3de3cb17c7372692667
Rhaven Kurosaia <105242602+Rhaven3@users.noreply.github.com>
2024-06-14 13:30:40 +0200

    Restart for Proto2

---
+++ extractor.cpp
@@ -0,0 +1,12 @@
+#include "extractor.h"
+
+Extractor::Extractor() {}
+
+//extractLogD
+void Extractor::ExtractLogD() {
+
+}
+    //extractOF
+void Extractor::ExtractOF(){
+
+}
---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+//src
+#include "mainwindow.h"
+//default
+#include <QApplication>
+#include <iostream>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+
+
+    w.show();
+    return a.exec();
+}
